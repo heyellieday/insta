12505cc4e01402a755c0e7b420d0f1311c0b4109
heyellieday <heyellieday@gmail.com>
2014-05-07 00:24:49 +0000

    Add droplet creation

---
+++ insta/config/initializers/api_keys.rb
@@ -0,0 +1,4 @@
+require 'digitalocean'
+
+Digitalocean.client_id  = "63dd809ecea5e910df77f5a11e4b2dee"
+Digitalocean.api_key    = "3028f1fb01f9a42a1c51401a5f0e718b"
--- insta/db/schema.rb
+++ insta/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506032026) do
+ActiveRecord::Schema.define(version: 20140507032026) do
 
   create_table "identities", force: true do |t|
     t.integer  "user_id"
@@ -67,10 +67,12 @@ ActiveRecord::Schema.define(version: 20140506032026) do
     t.string   "name"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username"
 
 end
